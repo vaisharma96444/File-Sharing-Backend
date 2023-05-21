58f411e3bb9ebb9cac17ee9854e210b109e57735
Vaibhav <vaisharma96444@gmail.com>
2023-05-21 18:52:55 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+
+const DBConnection = async () => {
+   
+
+const MONGO_URI= process.env.DATABASE
+
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
---
+++ index.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+import express from 'express';
+import router from './routes/routes.js';
+import cors from 'cors';
+import DBConnection from './database/db.js';
+
+
+
+
+const app = express();
+
+app.use('/',cors());
+app.use('/',router);
+
+const PORT = (process.env.PORT || 8000);
+
+DBConnection();
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
